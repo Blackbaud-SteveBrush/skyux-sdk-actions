ce13b130e302071c05f92db04480d89caa9f08b3
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-06-10 17:33:10 -0400

    Use pipe

--- dist/index.js
+++ dist/index.js
@@ -1178,68 +1178,39 @@ var __importStar = (this && this.__importStar) || function (mod) {
     result["default"] = mod;
     return result;
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(__webpack_require__(470));
-const cross_spawn_1 = __importDefault(__webpack_require__(20));
+const cross_spawn_1 = __webpack_require__(20);
 const path = __importStar(__webpack_require__(622));
 function spawn(command, args = []) {
     return __awaiter(this, void 0, void 0, function* () {
-        const childProcess = cross_spawn_1.default.sync(command, args, {
-            stdio: 'inherit',
+        const childProcess = cross_spawn_1.spawn(command, args, {
+            stdio: 'pipe',
             cwd: path.resolve(process.cwd(), core.getInput('working-directory'))
         });
-        return Promise.resolve(childProcess.stdout.toString());
-        // const spawn = require('child_process').spawn;
-        // // Create a child process
-        // var child = spawn(command , args, {
-        //   stdio: 'inherit',
-        //   cwd: path.resolve(process.cwd(), core.getInput('working-directory'))
-        // });
-        // return new Promise((resolve, reject) => {
-        //   child.stdout.on('data', function (data: any) {
-        //     console.log('ls command output: ' + data);
-        //     resolve(data);
-        //   });
-        //   child.stderr.on('data', function (data: any) {
-        //     //throw errors
-        //     console.log('stderr: ' + data);
-        //     reject(data);
-        //   });
-        //   child.on('close', function (code: any) {
-        //     console.log('child process exited with code ' + code);
-        //   });
-        // });
-        // const childProcess = crossSpawn(command, args, {
-        //   stdio: 'inherit',
-        //   cwd: path.resolve(process.cwd(), core.getInput('working-directory'))
-        // });
-        // return new Promise((resolve, reject) => {
-        //   let output: string;
-        //   if (childProcess.stdout) {
-        //     console.log('There\'s an stdout!!!!!');
-        //     childProcess.stdout.on('data', (data) => {
-        //       output = data.toString('utf8');
-        //       console.log('CHILD SPAWN OUTPUT!', output);
-        //     });
-        //   }
-        //   let errorMessage: string;
-        //   if (childProcess.stderr) {
-        //     childProcess.stderr.on('data', (data) => {
-        //       errorMessage = data.toString('utf8');
-        //     });
-        //   }
-        //   childProcess.on('error', (err) => reject(err));
-        //   childProcess.on('exit', (code) => {
-        //     if (code === 0) {
-        //       resolve(output);
-        //     } else {
-        //       reject(errorMessage);
-        //     }
-        //   });
-        // });
+        return new Promise((resolve, reject) => {
+            let output;
+            if (childProcess.stdout) {
+                childProcess.stdout.on('data', (data) => {
+                    output = data.toString('utf8');
+                });
+            }
+            let errorMessage;
+            if (childProcess.stderr) {
+                childProcess.stderr.on('data', (data) => {
+                    errorMessage = data.toString('utf8');
+                });
+            }
+            childProcess.on('error', (err) => reject(err));
+            childProcess.on('exit', (code) => {
+                if (code === 0) {
+                    resolve(output);
+                }
+                else {
+                    reject(errorMessage);
+                }
+            });
+        });
     });
 }
 exports.spawn = spawn;
--- src/spawn.ts
+++ src/spawn.ts
@@ -1,70 +1,39 @@
 
 import * as core from '@actions/core';
-import crossSpawn from 'cross-spawn';
+import { spawn as crossSpawn } from 'cross-spawn';
 import * as path from 'path';
 
 export async function spawn(command: string, args: string[] = []): Promise<string> {
-  const childProcess = crossSpawn.sync(command, args, {
-    stdio: 'inherit',
+
+  const childProcess = crossSpawn(command, args, {
+    stdio: 'pipe',
     cwd: path.resolve(process.cwd(), core.getInput('working-directory'))
   });
 
-  return Promise.resolve(childProcess.stdout.toString());
+  return new Promise((resolve, reject) => {
 
-  // const spawn = require('child_process').spawn;
-  // // Create a child process
-  // var child = spawn(command , args, {
-  //   stdio: 'inherit',
-  //   cwd: path.resolve(process.cwd(), core.getInput('working-directory'))
-  // });
+    let output: string;
+    if (childProcess.stdout) {
+      childProcess.stdout.on('data', (data) => {
+        output = data.toString('utf8');
+      });
+    }
 
-  // return new Promise((resolve, reject) => {
-  //   child.stdout.on('data', function (data: any) {
-  //     console.log('ls command output: ' + data);
-  //     resolve(data);
-  //   });
+    let errorMessage: string;
+    if (childProcess.stderr) {
+      childProcess.stderr.on('data', (data) => {
+        errorMessage = data.toString('utf8');
+      });
+    }
 
-  //   child.stderr.on('data', function (data: any) {
-  //     //throw errors
-  //     console.log('stderr: ' + data);
-  //     reject(data);
-  //   });
+    childProcess.on('error', (err) => reject(err));
 
-  //   child.on('close', function (code: any) {
-  //     console.log('child process exited with code ' + code);
-  //   });
-  // });
-
-  // const childProcess = crossSpawn(command, args, {
-  //   stdio: 'inherit',
-  //   cwd: path.resolve(process.cwd(), core.getInput('working-directory'))
-  // });
-
-  // return new Promise((resolve, reject) => {
-  //   let output: string;
-  //   if (childProcess.stdout) {
-  //     console.log('There\'s an stdout!!!!!');
-  //     childProcess.stdout.on('data', (data) => {
-  //       output = data.toString('utf8');
-  //       console.log('CHILD SPAWN OUTPUT!', output);
-  //     });
-  //   }
-
-  //   let errorMessage: string;
-  //   if (childProcess.stderr) {
-  //     childProcess.stderr.on('data', (data) => {
-  //       errorMessage = data.toString('utf8');
-  //     });
-  //   }
-
-  //   childProcess.on('error', (err) => reject(err));
-
-  //   childProcess.on('exit', (code) => {
-  //     if (code === 0) {
-  //       resolve(output);
-  //     } else {
-  //       reject(errorMessage);
-  //     }
-  //   });
-  // });
+    childProcess.on('exit', (code) => {
+      if (code === 0) {
+        resolve(output);
+      } else {
+        reject(errorMessage);
+      }
+    });
+  });
 }
